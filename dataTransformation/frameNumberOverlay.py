e424583846d77fe2c1281cb6da0312c6bb94e783
Daniel Müller <email.daniel.ch@gmail.com>
2020-11-03 14:28:32 +0100

    Create sub pipeline for frame overlay

--- dataTransformation/frameNumberOverlay.py
+++ dataTransformation/frameNumberOverlay.py
@@ -1,7 +1,33 @@
 
-import glob, os, sys
+import glob
+import ffmpeg
+import os
 
-for file in glob.glob('Video/*.mp4'):
-    os.system(f'ffmpeg -i {file} -vf "drawtext=fontfile=Arial.ttf: text='%{frame_num}': start_number=1: x=(w-tw)/2: y=h-(2*lh): fontcolor=black: fontsize=20: box=1: boxcolor=white: boxborderw=5" -c:a copy {file}')
+for file in glob.glob('../data/*.mp4'):
+
+    # Define where to read and where to write
+    pathInput = file
+    pathOutput = os.path.basename(file)
+
+    # Specify process steps
+    stream = ffmpeg.input(pathInput)
+    stream = ffmpeg.drawtext(
+        stream,
+        text="%{n}",
+        start_number=1,
+        escape_text=False, # This allows for text to be dynamically changed
+        x=20,
+        y=20,
+        fontcolor='black',
+        fontsize=20,
+        box=1,
+        boxcolor='white',
+        boxborderw=5
+        )
+    stream = ffmpeg.output(stream, pathOutput)
+
+    # Run on file
+    # stream.get_args() # Debug what ffmpeg does
+    stream.run()
 
     print(file)
