96ef7eefcab063b58991ca9470b18b01fa87ebfe
Daniel Müller <email.daniel.ch@gmail.com>
2020-11-03 20:57:18 +0100

    Add state for preselected index images

--- validation.py
+++ validation.py
@@ -5,9 +5,6 @@ import glob
 import cv2
 import SessionState
 
-
-
-
 # The data
 df = pd.DataFrame(
     {
@@ -19,15 +16,16 @@ df = pd.DataFrame(
 # The images
 images = glob.glob('data/*.jpeg')
 # https://discuss.streamlit.io/t/how-can-i-create-a-app-to-explore-the-images-in-a-folder/5458/2
-# To have a persistent index, use 'number_input'
-sessionState = SessionState.get(indexImage = 0)
+# https://discuss.streamlit.io/t/is-there-any-working-example-for-session-state-for-streamlit-version-0-63-1/4551
+sessionState = SessionState.get(
+    indexImage = 0,
+    currentAoi = 0)
 
 # Start blank app
 st.title('Blank template app')
 
 if st.button('Next image'):
     sessionState.indexImage += 1
-
 
 if st.button('Previous image'):
     if sessionState.indexImage > 0:
@@ -37,6 +35,11 @@ st.text(f'current image index: {sessionState.indexImage}')
 image = cv2.imread(images[int(sessionState.indexImage)])
 st.image(image, use_column_width=True, channels = 'BGR')
 
+
+aoi = st.radio(
+    'Chose on which AOI gaze is currently', 
+    [1, 2, 3, 4],
+    index = sessionState.currentAoi)
 
 
 # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
@@ -48,6 +51,6 @@ def get_table_download_link(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings/bytes conversions
     return f'<a href="data:file/txt;base64,{b64}" \
-        download="{csv}"><input type="button" value="Download"></a>'
+        download="{csv}"><input type="button" value="Download Results"></a>'
 
 st.markdown(get_table_download_link(df), unsafe_allow_html=True)
