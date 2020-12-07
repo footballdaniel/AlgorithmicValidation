d0f4fb4d38752762cfb83685d70d48836046a135
Daniel Müller <email.daniel.ch@gmail.com>
2020-12-07 22:14:13 +0100

    Update to df export

--- validation.py
+++ validation.py
@@ -15,7 +15,8 @@ sessionState = SessionState.get(
     currentAoi = 0,
     keyPassword = 0, # If the key != 0, the input field is empty/disabled. see:
     keyJump = 0,
-    tempFolder = "") # https://github.com/streamlit/streamlit/issues/623#issuecomment-551755236
+    tempFolder = "",
+    dataFrame = pd.DataFrame()) # https://github.com/streamlit/streamlit/issues/623#issuecomment-551755236
 
 # Create a session temporary folder if none has been defined
 if sessionState.tempFolder == "":
@@ -47,61 +48,55 @@ if (jumpTo):
     sessionState.indexImage = int(jumpTo)
     sessionState.keyJump += 1 # Reset the input field
 
-# The data
-df = pd.DataFrame(
-    {
-        'test': [1,2,3],
-        'values': [2,4,5]
-    }
-)
-
 # Start blank app
 st.title('Validation Gaze')
 st.text('This is the validation of the Pupil eye-tracker. It contains a total of 1600 frames.')
 
 # The images
 # images = glob.glob('data/*.jpeg')
-images = glob.glob(sessionState.tempFolder + "/*.jpeg")
+images = sorted(glob.glob(sessionState.tempFolder + "/*.jpeg"))
 
-st.text(images)
+# The UI
+if images != []:
 
-st.text(sessionState.tempFolder)
+    # st.text(images)
+    st.text("Loaded temporarily to: " + sessionState.tempFolder)
 
-# Two column buttons
-col1, col2= st.beta_columns(2)
+    # Two column buttons
+    col1, col2= st.beta_columns(2)
 
-with (col1):
-    if st.button('Previous image'):
-        if sessionState.indexImage > 0:
-            sessionState.indexImage -= 1
+    with (col1):
+        if st.button('Previous image'):
+            if sessionState.indexImage > 0:
+                sessionState.indexImage -= 1
 
-with (col2):
-    if st.button('Next image'):
-        sessionState.indexImage += 1
+    with (col2):
+        if st.button('Next image'):
+            sessionState.dataFrame = sessionState.dataFrame.append({'Frame': images[int(sessionState.indexImage)]}, ignore_index = True)
+            sessionState.indexImage += 1
 
+    # AOI button
+    aoi = st.radio(
+        'Chose on which AOI gaze is currently', 
+        ['Head', 'Chest', 'Pelvis', 'Left arm', 'Right arm', 'Left leg', 'Right leg', 'Other'],
+        index = sessionState.currentAoi)
 
-# AOI button
-aoi = st.radio(
-    'Chose on which AOI gaze is currently', 
-    ['Head', 'Chest', 'Pelvis', 'Left arm', 'Right arm', 'Left leg', 'Right leg', 'Other'],
-    index = sessionState.currentAoi)
+    # Display current image
+    image = cv2.imread(images[int(sessionState.indexImage)])
+    st.image(image, use_column_width=True, channels = 'BGR')
 
-# Display current image
-image = cv2.imread(images[int(sessionState.indexImage)])
-st.image(image, use_column_width=True, channels = 'BGR')
+    # https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+    def get_table_download_link(df):
+        """Generates a link allowing the data in a pandas dataframe to be downloaded
+        in:  dataframe
+        out: href string
+        """
+        csv = df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()  # strings/bytes conversions
+        return f'<a href="data:file/txt;base64,{b64}" \
+            download="{csv}"><input type="button" value="Download Results"></a>'
 
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def get_table_download_link(df):
-    """Generates a link allowing the data in a pandas dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings/bytes conversions
-    return f'<a href="data:file/txt;base64,{b64}" \
-        download="{csv}"><input type="button" value="Download Results"></a>'
-
-# Index and save
-st.text(f'current image index: {sessionState.indexImage}')
-st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+    # Index and save
+    st.text(f'current image index: {sessionState.indexImage}')
+    st.markdown(get_table_download_link(sessionState.dataFrame), unsafe_allow_html=True)
 
