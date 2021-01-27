2e35fc783eea840ac3301a678438e15cb3a5a1b5
Daniel Müller <email.daniel.ch@gmail.com>
2021-01-27 12:19:09 +0100

    Move interpretation scripts, set logo location, minimize dockerimage

--- _dataInterpretation/MakePDF.sh
+++ _dataInterpretation/MakePDF.sh
@@ -1,1 +1,12 @@
-pandoc -s summary.md -t context -o summary.pdf
+echo Requires PANDOC to be installed
+echo Requires SVGCONVERT to be installed
+echo Requires TEX e.g. MIKTEX to be installed
+
+echo === START COMPILING DOCUMENT ===
+
+pandoc -s summary.md -o summary.pdf
+
+echo === FINISH COMPILING DOCUMENT ===
+
+# Opens shell and prevents closing on error
+$SHELL
