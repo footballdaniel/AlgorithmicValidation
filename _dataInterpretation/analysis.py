9a8b2aa7a6423e18d303cbeed185989a98c22283
Daniel Müller <email.daniel.ch@gmail.com>
2021-03-30 20:04:44 +0200

    data loader working, added jupyter reader

--- _dataInterpretation/analysis.py
+++ _dataInterpretation/analysis.py
@@ -4,8 +4,9 @@ from analysis.services.preprocess import DataLoader
 algorithmic_files = "analysis/tests/data/P*.txt"
 rater_files = "analysis/tests/data/data_*.csv"
 
+# Load with TDD approach (Merging is still incompletely happening)
 data = DataLoader().load(algorithmic_files, rater_files)
 
-
+# Load with Jupyter approach
 
 a = 1
