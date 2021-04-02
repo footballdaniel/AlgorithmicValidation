1fe577ffc4b3c94b8d440b102bae736c24fd7bde
Daniel Müller <email.daniel.ch@gmail.com>
2021-04-02 12:13:18 +0200

    Update workflow file, add latest images

--- _dataInterpretation/analysis.py
+++ _dataInterpretation/analysis.py
@@ -1,4 +1,5 @@
 # All data analysis for the algorithmic validation
+from analysis.services.jupyter_reader import JupyterReader
 from analysis.services.preprocess import DataLoader
 
 algorithmic_files = "analysis/tests/data/P*.txt"
@@ -8,5 +9,8 @@ rater_files = "analysis/tests/data/data_*.csv"
 data = DataLoader().load(algorithmic_files, rater_files)
 
 # Load with Jupyter approach
+reader = JupyterReader()
+reader.load(algorithmic_files, rater_files)
+data = reader.df_merged
 
 a = 1
